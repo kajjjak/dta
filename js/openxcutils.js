86db83401d195f0fa5d9f0ad330033bcada62140
kajjjak <mail@kjartanjonsson.com>
2013-07-21 12:35:50 -0700

    Adding simulation

--- js/openxcutils.js
+++ js/openxcutils.js
@@ -5,13 +5,15 @@ function OpenXCReader(){
 		//expects json data having format
 		// {"timestamp": 1351181576.64078, "name": "engine_speed", "value": 714.0}
 		// where name can have different properties
+		var m;
 		var buff = this.buffer[line.name];
 		if(!buff){ buff = []; }
 		if(buff[buff.length - 1] != line.value){buff.push(line.value);}
 		if (this.hasReadLineFlush(line.name)){
-			this.flush();
+			m = this.flush();
 		}
 		this.buffer[line.name] = buff;
+		return m; 
 	};
 	
 	this.hasReadLineFlush = function(attr){
@@ -23,7 +25,9 @@ function OpenXCReader(){
 	
 	this.flush = function(){
 		console.info("saving " + JSON.stringify(this.buffer));
+		var m = window.vehicle_readings.create(this.buffer);
 		this.buffer = {};
+		return m;
 	};
 }
 
