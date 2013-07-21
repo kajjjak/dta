c9a949ea769a63aeaebc59de7659015914bdb9ae
kajjjak <mail@kjartanjonsson.com>
2013-07-21 12:35:28 -0700

    Adding simulation

--- js/models.js
+++ js/models.js
@@ -1,24 +1,6 @@
 // http://find-a-driving-school.ca/top-tips-reducing-fuel-consumption/
 
-/*
-	latitude
-	longitude
-	vehicle_speed
-	engine_speed
-	odometer
-	headlamp_status 
-	high_beam_status
-	fuel_consumed_since_restart
-	fuel_level
-	parking_brake_status
-	brake_pedal_status
-	accelerator_pedal_position
-*/
-
-// Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
-
-  // Enable PouchDB debugging
   // Pouch.DEBUG = true;
 
   Backbone.sync = BackbonePouch.sync({
@@ -41,17 +23,9 @@ $(function(){
 
   });
 
-  // VehicleReading Collection
-  // ---------------
-
-  // The collection of VehicleReadings is backed by *PouchDB* instead of a remote
-  // server.
   var VehicleReadingList = Backbone.Collection.extend({
-
-    // Reference to this collection's model.
     model: VehicleReading,
 
-    // Include docs in Map Reduce response. Order by `order`.
     pouch: {
       fetch: 'query',
       options: {
@@ -66,24 +40,12 @@ $(function(){
     }
   });
 
-  // Create our global collection of **VehicleReadings**.
   window.vehicle_readings = new VehicleReadingList;
 
-  // The Application
-  // ---------------
-
-  // Our overall **AppView** is the top-level piece of UI.
   var AppView = Backbone.View.extend({
 
-    // Instead of generating a new element, bind to the existing skeleton of
-    // the App already present in the HTML.
-    // Delegated events for creating new items, and clearing completed ones.
     events: {
     },
-
-    // At initialization we bind to the relevant events on the `VehicleReadings`
-    // collection, when items are added or changed. Kick things off by
-    // loading any preexisting vehicle_readings that might be saved in *PouchDB*.
     initialize: function() {
 
       vehicle_readings.bind('add', this.addOne, this);
@@ -93,23 +55,16 @@ $(function(){
       vehicle_readings.fetch();
     },
 
-    // Re-rendering the App just means refreshing the statistics -- the rest
-    // of the app doesn't change.
     render: function() {
 
       if (vehicle_readings.length) {
-      	
       }
-
     },
 
-    // Add a single vehicle_reading item to the list by creating a view for it, and
-    // appending its element to the `<ul>`.
     addOne: function(vehicle_reading) {
       
     },
 
-    // Add all items in the **VehicleReadings** collection at once.
     addAll: function() {
       vehicle_readings.each(this.addOne);
     },
@@ -123,9 +78,6 @@ $(function(){
     }
 
   });
-
-  // Finally, we kick things off by creating the **App**.
-  window.vehicle_data_view = new AppView;
-	
+  window.vehicle_data_view = new AppView;	
 });
 
