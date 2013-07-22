fa9989d703595f05ccab2b90b6b7ff3aefff873e
kajjjak <mail@kjartanjonsson.com>
2013-07-22 04:52:11 -0700

    Simple gauges

--- js/map-load.js
+++ js/map-load.js
@@ -20,14 +20,14 @@ function showDriversPath(){
 		addDriversPath(m);
 	});
 }
-
+green_factor = 1;
 function addDriversPath(m){
 	var speed = m.attributes.vehicle_speed[0];
 	var lat = m.attributes.latitude[0];
 	var lng = m.attributes.longitude[0];
-	var opacity = speed/80;
+	var opacity = green_factor;//speed/80;
 	//console.info("color " + 255*(speed/80));
-	map.appendLineToPosition(lat, lng, "driver_path", {"color": "blue", "opacity": opacity});	
+	map.appendLineToPosition(lat, lng, "driver_path", {"color": "darkgreen", "opacity": opacity});	
 }
 
 function stopDriversPathSimulation(){
@@ -42,8 +42,8 @@ function howGreen(d){
 	window.vehicle_data_read = window.vehicle_data_read || {};
 	var gears = {"first":1, "second":2, "third":3, "fourth":4, "fifth":5, "sixth":6, "seventh":7, "eighth":8, "reverse":1, "neutral":0};
 	var gears_efficency = {0:0.87, 1:0.722, 2:0.809, 3:0.87, 4:0.87, 5:0.87, 6:0.87, 7:0.87, 8:0.87}; // based on Bishop & Kluger, 1996
-	var gear_efficiency = gears_efficency[gears[d.transmission_gear_position]] + 13; //scale upp to 1
-	var rpm_efficiency = vehicle_data_read.gear_efficiency;
+	var gear_efficiency = gears_efficency[gears[d.transmission_gear_position]] - 0.722; //scale upp to 1
+	var rpm_efficiency = gear_efficiency;
 	var overal_efficiency = (gear_efficiency+rpm_efficiency)/2.0;
 	
 	if (d.accelerator_pedal_position){
@@ -64,21 +64,22 @@ function runDriversPathSimulation(vehicle_data_collection){
 	map.clearLayer("driver_path");
 	
 	carReader.onDataRead = function(d){
-		drawGauge(100*(d.vehicle_speed/200),100*(d.engine_speed/200),d.fuel_level );
-		$("#vehicle_simulation_step").html("Step: " + vehicle_simulation_index + " (" + (parseInt(1000*vehicle_simulation_index/vehicle_simulation_length))/1000 + " %)");
+		green_factor = howGreen(d);
+		drawGauge(d.vehicle_speed,d.engine_speed,d.fuel_level);
+		$("#vehicle_simulation_step").html("Step: " + vehicle_simulation_index + " (" + (parseInt(1000*vehicle_simulation_index/vehicle_simulation_length))/10 + " %)");
 		$("#vehicle_speed").html("vehicle_speed: " + d.vehicle_speed);
-		$("#vehicle_green").html("vehicle_green: " + howGreen(d));
-		/*
-		$("#torque_at_transmission").html("torque_at_transmission: " + d.torque_at_transmission);
-		$("#parking_brake_status").html("parking_brake_status: " + d.parking_brake_status);
-		$("#transmission_gear_position").html("transmission_gear_position: " + d.transmission_gear_position);
-		$("#odometer").html("odometer: " + d.odometer);
-		$("#ignition_status").html("ignition_status: " + d.ignition_status);
+		$("#vehicle_green").html("vehicle_green: " + green_factor);
 		$("#fuel_consumed_since_restart").html("fuel_consumed_since_restart: " + d.fuel_consumed_since_restart);
-		$("#fuel_level").html("fuel_level: " + d.fuel_level);
+		$("#ignition_status").html("ignition_status: " + d.ignition_status);
 		$("#headlamp_status").html("headlamp_status: " + d.headlamp_status);
 		$("#high_beam_status").html("high_beam_status: " + d.high_beam_status);
 		$("#brake_pedal_status").html("brake_pedal_status: " + d.brake_pedal_status);
+		$("#parking_brake_status").html("parking_brake_status: " + d.parking_brake_status);
+		/*
+		$("#torque_at_transmission").html("torque_at_transmission: " + d.torque_at_transmission);
+		$("#transmission_gear_position").html("transmission_gear_position: " + d.transmission_gear_position);
+		$("#odometer").html("odometer: " + d.odometer);
+		$("#fuel_level").html("fuel_level: " + d.fuel_level);
 		$("#fine_odometer_since_restart").html("fine_odometer_since_restart: " + d.fine_odometer_since_restart);
 		$("#accelerator_pedal_position").html("accelerator_pedal_position: " + d.accelerator_pedal_position);
 		*/
